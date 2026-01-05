8782edbddcf4a98c2598bf6c5cf2111c5a2a48e7
Malacca <malacca@ADMIN-02s-iMac.local>
2026-01-05 10:03:10 +0800

    session 01

---
+++ month01/week00/session01/Hello.java
@@ -0,0 +1,11 @@
+package mounth01.week00.session01;
+public class Hello {
+    public static void main (String[] args){
+        System.out.println("hello world");
+        System.out.println("Khuslen");
+        System.out.println("23+3");
+        System.out.println("devloper");
+        System.out.println(23+3);
+
+    }
+}
